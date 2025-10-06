1a875e87f3ff1abbb667b70310fd2d459cc7e6e3
23prime <23.prime.37@gmail.com>
2025-10-06 14:01:38 +0900

    Change title

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,6 +1,6 @@
 export const SITE_CONFIG = {
   name: "23prime",
-  title: "Software Engineer",
+  title: "Software Developer",
   profileImage: "/23prime.png",
   links: {
     github: "https://github.com/23prime",
