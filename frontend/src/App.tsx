ac169db49c64c6e9a4c8e52eee8931395ee20bd4
23prime <23.prime.37@gmail.com>
2025-10-06 14:06:58 +0900

    Replace Magic String with Symbolic Constant

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "@/components/ThemeProvider";
+import { ThemeProvider, THEMES } from "@/components/ThemeProvider";
 import { Layout } from "@/components/Layout";
 import { Home } from "@/pages/Home";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="23prime-theme">
+    <ThemeProvider defaultTheme={THEMES.SYSTEM} storageKey="23prime-theme">
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
